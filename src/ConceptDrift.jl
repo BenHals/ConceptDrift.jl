e2e3316f3efa39502964cd49bedb8d7acac02562
Ben Halstead <benhalstd@gmail.com>
2022-07-13 14:27:43 +1200

    Update ConceptDrift.jl

--- src/ConceptDrift.jl
+++ src/ConceptDrift.jl
@@ -1,12 +1,12 @@
 module ConceptDrift
 
-include("data\\stream.jl")
-include("data\\sine_generator.jl")
+include("data/stream.jl")
+include("data/sine_generator.jl")
 include("gaussian_estimator.jl")
 include("numeric_attribute_observer.jl")
 include("naive_bayes.jl")
 include("hoeffding_tree.jl")
-include("data\\random_tree_generator.jl")
+include("data/random_tree_generator.jl")
 
 
 end
